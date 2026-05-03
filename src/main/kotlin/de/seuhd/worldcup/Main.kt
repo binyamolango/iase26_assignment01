1c0e6eaf378d2ffd3db438cf0c2f29888f0a167f
Binyam Yohannes <125895809+binyamolango@users.noreply.github.com>
2026-05-03 18:02:23 +0200

    Implement interactive menu

--- src/main/kotlin/de/seuhd/worldcup/Main.kt
+++ src/main/kotlin/de/seuhd/worldcup/Main.kt
@@ -12,6 +12,29 @@ fun main() {
     val worldCupData = Json.decodeFromString<WorldCupData>(jsonText)
 
     //TODO: Implement interactive menu
+    while (true) {
+        println()
+        println("======= World Cup 2026 Menu =======")
+        println("1. Show Standings")
+        println("2. Show Matches")
+        println("3. Place Bets")
+        println("4. Show Betting Score")
+        println("0. Exit")
+        print("Choose an option: ")
+
+        when (readln()) {
+            "1" -> showStandings(worldCupData.groups)
+            "2" -> showMatches(worldCupData.groups)
+            "3" -> placeBets(worldCupData.groups)
+            "4" -> showBettingScore(worldCupData.groups)
+            "0" -> {
+                println("Thank you. Best of luck!!")
+                return
+            }
+            else -> println("Invalid Option. Please enter a valid option")
+        }
+    }
+
 }
 
 /* -------------------------------------------------------------
